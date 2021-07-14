7e9359db21119140e735a54ea99c27b480788130
Christian Rades <christian.rades@googlemail.com>
2021-07-14 15:40:03 +0200

    cleaned up & use eyre

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,13 @@
 use std::fs;
 use std::io;
 use std::time;
-use std::path::Path;
-use std::path::PathBuf;
+use std::hash::{Hash,Hasher};
+use std::path::{PathBuf, Path};
 use zip::ZipArchive;
 use argh::FromArgs;
+use color_eyre::eyre::Result;
+use color_eyre::eyre::eyre;
+use time::SystemTime;
 
 #[derive(FromArgs)]
 /// Shell into zips
@@ -17,76 +20,80 @@ struct Params {
 }
 
 
-fn main() {
-    let params: Params = argh::from_env();
-    std::process::exit(zipedit(params))
+fn main() -> Result<()> {
+    color_eyre::install()?;
+    let params = get_params()?;
+    zipedit(params)?;
+    Ok(())
 }
 
-impl Params {
-fn target(&self) -> Option<PathBuf> {
-    let target = self.target.clone();
-    if target.is_absolute() {
-        return Some(target);
+fn get_params() -> Result<Params> {
+    let mut params: Params = argh::from_env();
+    if params.target.is_absolute() {
+        return Ok(params);
     }
 
-    let cwd = std::env::current_dir().ok()?;
-    let target = cwd.join(target);
+    let cwd = std::env::current_dir()?;
+    let target = cwd.join(params.target);
     if target.is_file() {
-        return Some(target);
+        params.target = target;
+        return Ok(params);
+    }
+    Err(eyre!("No such file \"{}\"", target.to_string_lossy()))
+}
+
+fn zipedit(params: Params) -> Result<()>{
+    let work_dir = get_temp_work_dir(&params.target.to_string_lossy())?;
+
+    unpack_to_dir(&params.target, &work_dir)?;
+
+    if let Some(cmd) = params.command {
+        std::process::Command::new("bash")
+            .current_dir(&work_dir)
+            .arg("-c")
+            .arg(cmd)
+            .status();
+    } else {
+        let shell = std::env::var("SHELL").unwrap_or("sh".into());
+        std::process::Command::new(shell).current_dir(&work_dir).status();
     }
 
-    None
+    fs::remove_dir_all(work_dir)?;
+
+    Ok(())
 }
+
+fn get_temp_work_dir(path: &str) -> Result<PathBuf> {
+    let tmp_path = std::env::temp_dir();
+    let timestamp = SystemTime::now()
+        .duration_since(time::UNIX_EPOCH)
+        .map(|x| x.as_millis())?;
+    let mut hasher = std::collections::hash_map::DefaultHasher::new();
+    path.hash(&mut hasher);
+
+    Ok(tmp_path.join(format!("zipedit_{}_{:x}",timestamp, hasher.finish())))
 }
 
-fn zipedit(params: Params) -> i32 {
-    let mem_path = Path::new("/tmp/");
-    if ! mem_path.is_dir() {
-        return 2
-    }
-    let ts = time::SystemTime::now().duration_since(time::UNIX_EPOCH)
-        .and_then(|x| Ok(x.as_secs()))
-        .and_then(|x| Ok(x.to_string()))
-        .unwrap();
+fn unpack_to_dir(zip: &Path, dest_dir: &Path) -> Result<()> {
+    let zip_file = fs::File::open(zip)?;
 
-    let work_dir = &mem_path.join(format!("zipedit_{}",ts)); 
-    fs::create_dir(work_dir).unwrap();
+    let mut archive = ZipArchive::new(zip_file)?;
 
-    let target_file = &params.target().unwrap();
-    let zip_file = fs::File::open(target_file).unwrap();
-
-    let mut archive = ZipArchive::new(zip_file).unwrap();
-    
     for i in 0..archive.len() {
-        let mut file = archive.by_index(i).unwrap();
-        let dest = work_dir.join(file.sanitized_name());
+        let mut file = archive.by_index(i)?;
+        let dest = dest_dir.join(file.sanitized_name());
 
         if file.is_dir() {
-            fs::create_dir_all(dest).unwrap();
+            fs::create_dir_all(dest)?;
             continue
         }
         if let Some(parent) = dest.parent() {
             if !parent.exists() {
-                fs::create_dir_all(parent).unwrap();
+                fs::create_dir_all(parent)?;
             }
         }
-        let mut outfile = fs::File::create(dest).unwrap();
-        io::copy(&mut file, &mut outfile).unwrap();
+        let mut outfile = fs::File::create(dest)?;
+        io::copy(&mut file, &mut outfile)?;
     }
-
-    if let Some(cmd) = params.command {
-        std::process::Command::new("bash")
-            .current_dir(work_dir)
-            .arg("-c")
-            .arg(cmd)
-            .status()
-            .unwrap();
-    } else {
-        let shell = std::env::var("SHELL").unwrap_or("sh".into());
-        std::process::Command::new(shell).current_dir(work_dir).status().unwrap();
-    }
-
-    fs::remove_dir_all(work_dir).unwrap();
-
-    0
+    Ok(())
 }
